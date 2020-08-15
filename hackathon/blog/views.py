130045683a69b8d02ff7a2b59ea57618ea25c8fe
HyeonMin kim <c3man4@naver.com>
2020-08-15 17:33:50 +0900

    first

--- hackathon/blog/views.py
+++ hackathon/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'blog/index.html')
+
+def login(request):
+    return render(request,'blog/login.html')
+
+def main(request):
+    return render(request,'blog/main.html')
+
+def signup(request):
+    return render(request,'blog/signup.html')
