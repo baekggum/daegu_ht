4c52e637aaa359d98f8fd2689e9aa2dec4b2334f
hyegyo <seohyegyo@naver.com>
2020-08-23 17:09:14 +0900

    자기 냉장고 뜨게

--- kitchenproject/blog/views.py
+++ kitchenproject/blog/views.py
@@ -5,16 +5,24 @@ from .models import Users
 from receipe.models import Ingredient
 from django.http import JsonResponse
 
+
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 
-
 def refrigerator(request):
-    if 'term' in request.GET:
-        qs = Ingredient.objects.filter(name__istartswith=request.GET.get('term'))
-        food_ingredient = list()
-        for product in qs:
-            food_ingredient.append(product.name)
-        return JsonResponse(food_ingredient, safe=False)
-    return render(request,'refrigerator.html')
+    # blog_detail=get_object_or_404(Users,pk=blog_id)
+    current_user = str(request.user)
+    print(type(current_user))
+    print(current_user)
+    for x in Users.objects.all():
+   # x.username이 Users_name_list에 이미 존재하면, 다시 반복문 실행 
+        print(type(x.name))
+        if x.name==current_user:
+            context={
+            "name": x.name,
+            "food_ingredient": x.food_ingredient,
+            "food_seasoning": x.food_seasoning,
+             }
+        
+    return render(request,"refrigerator.html",context=context)
