eb3105f4a4f672e4f814366fb3abb21dce994fdd
BaekGGum <c3man4@naver.com>
2020-08-24 16:51:20 +0900

    plus front

--- kitchenproject/blog/models.py
+++ kitchenproject/blog/models.py
@@ -4,11 +4,7 @@ from jsonfield import JSONField
 # Create your models here.
 
 class Users(Model):
-<<<<<<< HEAD
-    name=CharField(max_length=20)
-=======
     name=CharField(default=True,max_length=20)
->>>>>>> 4c52e637aaa359d98f8fd2689e9aa2dec4b2334f
     food_seasoning=ListCharField(
         default=True,
         base_field=CharField(max_length=10),
--- kitchenproject/blog/views.py
+++ kitchenproject/blog/views.py
@@ -10,6 +10,12 @@ def home(request):
     return render(request,'home.html')
 
 def refrigerator(request):
+    if 'term' in request.GET:
+        qs = Ingredient.objects.filter(name__istartswith=request.GET.get('term'))
+        food_ingredient = list()
+        for product in qs:
+            food_ingredient.append(product.name)
+        return JsonResponse(food_ingredient, safe=False)
     # blog_detail=get_object_or_404(Users,pk=blog_id)
     current_user = str(request.user)
     print(type(current_user))
